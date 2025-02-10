bf1ecabcf25f8e1bd0e50042b1fea5ea7274456c
Maiconfz <maiconfz@gmail.com>
2025-02-10 02:11:39 +0000

    Separate 2json and 2Spreadsheet parsers

--- src/main/java/io/github/doc2json/doc2json_core/doc_parser/spreadsheet/Xlsx2JsonParser.java
+++ src/main/java/io/github/doc2json/doc2json_core/doc_parser/spreadsheet/Xlsx2JsonParser.java
@@ -1,20 +1,14 @@
 package io.github.doc2json.doc2json_core.doc_parser.spreadsheet;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Set;
 
-import org.apache.commons.io.FilenameUtils;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.springframework.stereotype.Component;
 
 import com.google.gson.Gson;
 
 import io.github.doc2json.doc2json_core.doc_parser.Doc2JsonParser;
-import io.github.doc2json.doc2json_core.mapper.spreadsheet.apache_poi.ApachePoiWorkbookToDoc2JsonSpreadsheetMapper;
-import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -23,22 +17,16 @@ import lombok.Getter;
 @Getter
 public class Xlsx2JsonParser implements Doc2JsonParser {
 
-    private final Set<String> supportedExtensions = Set.of("xlsx", "xls");
-
-    @Getter(AccessLevel.NONE)
-    private final ApachePoiWorkbookToDoc2JsonSpreadsheetMapper mapper;
+    private final Xlsx2SpreadsheetParser xlsx2SpreadSheetParser;
 
     @Override
     public String toJson(File file) throws IOException {
-        String fileExtension = FilenameUtils.getExtension(file.getName());
+        return new Gson().toJson(xlsx2SpreadSheetParser.toSpreadsheet(file));
+    }
 
-        if (!supportedExtensions.contains(fileExtension)) {
-            throw new IllegalArgumentException("Unsupported file extension: " + fileExtension);
-        }
-
-        try (FileInputStream fis = new FileInputStream(file); final Workbook workbook = WorkbookFactory.create(fis);) {
-            return new Gson().toJson(mapper.toDoc2JsonSpreadsheet(workbook));
-        }
+    @Override
+    public Set<String> getSupportedExtensions() {
+        return xlsx2SpreadSheetParser.getSupportedExtensions();
     }
 
 }
